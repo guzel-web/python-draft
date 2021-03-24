904bbbbea95cfa1451cdd6a49c434968c451bce6
guzel-web <guzel_sh07@mail.ru>
2021-03-24 16:43:11 +0300

    Rename

--- simple_generator.py
+++ simple_generator.py
@@ -1,15 +1,34 @@
-def simple_gen():
-    print('Checkpoint 1')
-    yield 1
-    print('Checkpoint 2')
-    return 'No more elements' #Показывается при выбросе ошибки StopIteration: No more elements
-    yield  2 # при присутствии return, недостижимая операция
-    print('Checkpoint 3')
+# def simple_gen():
+#     print('Checkpoint 1')
+#     yield 1
+#     print('Checkpoint 2')
+#     return 'No more elements' #Показывается при выбросе ошибки StopIteration: No more elements
+#     yield  2 # при присутствии return, недостижимая операция
+#     print('Checkpoint 3')
+#
+# gen = simple_gen()
+# x = next(gen)
+# print(x)
+# y = next(gen)
+# print(y)
+# z = next(gen)
+# print(z)
 
-gen = simple_gen()
-x = next(gen)
-print(x)
-y = next(gen)
-print(y)
-z = next(gen)
-print(z)
+def simple():
+    a = 0
+    while a:
+        a += 1
+        yield a
+
+gen = simple(1)
+for i in gen:
+    print(i, end = ' ')
+
+# def random_generator(k): # делает то же самое, что RandomIterator
+#     # yield генератор вместо функции return,
+#     # может вернуть значении из функции несколько раз
+#     for i in range(k):
+#         yield random()
+# gen = random_generator(3)
+# for i  in gen:
+#     print(i)
